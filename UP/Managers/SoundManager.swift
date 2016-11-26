02d44501f484795fdcb39873e025924fcff4de3b
Seru <ghks1353@gmail.com>
2016-11-26 18:52:33 +0900

    Build 0.0.33 for gigdc (Enoch An sound included version)

--- UP/Managers/SoundManager.swift
+++ UP/Managers/SoundManager.swift
@@ -11,12 +11,12 @@ import Foundation
 class SoundManager {
 	
 	static var list:Array<SoundInfoObj> = [
-		SoundInfoObj(soundName: "Marble Soda", fileName: "sounds-alarms-test-marvelsoda.aiff"),
+		/*SoundInfoObj(soundName: "Marble Soda", fileName: "sounds-alarms-test-marvelsoda.aiff"),
 		SoundInfoObj(soundName: "Sapphire", fileName: "sounds-alarms-test-kari-sapphire.aiff"),
 		SoundInfoObj(soundName: "WANDERLUST", fileName: "sounds-alarms-test-wanderlust.aiff"),
 		SoundInfoObj(soundName: "The big black", fileName: "sounds-alarms-test-bigblack.aiff"),
-		SoundInfoObj(soundName: "바ㅏㅏ카야로오오오ㅗㅗ", fileName: "sounds-alarms-test-marvelsodamatsuda.aiff")
-		
+		SoundInfoObj(soundName: "바ㅏㅏ카야로오오오ㅗㅗ", fileName: "sounds-alarms-test-marvelsodamatsuda.aiff")*/
+		SoundInfoObj(soundName: "sounds-alarms-test-theseus", fileName: "sounds-alarms-test-theseus.aiff")
 	];
 	
 	//사운드 이름에 대한 실제 사운드 오브젝트 반환
